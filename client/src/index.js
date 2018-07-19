c948b12a8419a830e4d000fc99ccd80e6c6079de
Ray Templo <rtemplo@gmail.com>
2018-07-19 14:34:36 -0400

    cleaned up some comment code in index.js

--- client/src/index.js
+++ client/src/index.js
@@ -7,27 +7,14 @@ import registerServiceWorker from './registerServiceWorker'
 
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
-// import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { AUTH_TOKEN } from './constants'
-// import { ApolloLink, split } from 'apollo-client-preset'
 import { ApolloLink } from 'apollo-client-preset'
-// import { WebSocketLink } from 'apollo-link-ws'
-// import { getMainDefinition } from 'apollo-utilities'
 
 import { createHttpLink } from "apollo-link-http"
-// import { setContext } from "apollo-link-context"
 
 const httpLink = new createHttpLink({uri: 'http://localhost:3000/graphql'})
-
-// const authLink = token =>
-//   setContext((_, { headers }) => ({
-//     headers: {
-//       ...headers,
-//       ...(token ? { Authorization: `Bearer ${token}` } : {})
-//     }
-//   }));
 
 const middlewareAuthLink = new ApolloLink((operation, forward) => {
   const token = localStorage.getItem(AUTH_TOKEN)
@@ -46,30 +33,6 @@ const client = new ApolloClient({
   link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 });
-
-// const wsLink = new WebSocketLink({
-//   uri: `ws://localhost:3000`,
-//   options: {
-//     reconnect: true,
-//     connectionParams: {
-//       authToken: localStorage.getItem(AUTH_TOKEN),
-//     }
-//   }
-// })
-
-// const link = split(
-//   ({ query }) => {
-//     const { kind, operation } = getMainDefinition(query)
-//     return kind === 'OperationDefinition' && operation === 'subscription'
-//   },
-//   wsLink,
-//   httpLinkWithAuthToken,
-// )
-
-// const client = new ApolloClient({
-//   link,
-//   cache: new InMemoryCache()
-// })
 
 ReactDOM.render(
   <BrowserRouter>
